05286892d0edfcb3acce162e78c6a4584e870aa0
MrBaTTka <ilyha_96@inbox.ru>
2024-09-19 10:16:42 +0300

    add refresh btn

--- src/component/Header/Header.jsx
+++ src/component/Header/Header.jsx
@@ -9,19 +9,26 @@ const Header = () => {
             <ul>
                 <div className={style.button}>
                     <li>
-                        <NavLink to="/price-for-order" className={ navData => navData.isActive ? style.button_active_1 : style.button}>
+                        <NavLink to="/price-for-order" className={navData => navData.isActive ? style.button_active_1 : style.button}>
                             Под заказ
                         </NavLink>
                     </li>
                 </div>
                 <div className={style.button}>
-                <li>
-                    <NavLink to="/price-from-base" className={ navData => navData.isActive ? style.button_active_2 : style.button}>
-                        По наличию
-                    </NavLink>
-                </li>
-            </div>
-        </ul>
+                    <li>
+                        <NavLink to="/price-from-base" className={navData => navData.isActive ? style.button_active_2 : style.button}>
+                            По наличию
+                        </NavLink>
+                    </li>
+                </div>
+                <div className={style.wrapper_refresh}>
+                    <li>
+                        <a href="https://mrbattka.github.io/unimtrn/" className={style.refresh}>
+                            ↻
+                        </a>
+                    </li>
+                </div>
+            </ul>
 
         </nav >
     )
