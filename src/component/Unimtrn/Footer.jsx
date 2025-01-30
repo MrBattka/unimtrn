d8e2eb5a74c3b0f8d4fb8f37ed9ad6644c887542
MrBaTTka <ilyha_96@inbox.ru>
2025-01-30 10:10:35 +0300

    add footer

--- src/component/Unimtrn/Footer.jsx
+++ src/component/Unimtrn/Footer.jsx
@@ -6,8 +6,12 @@ const Footer = () => {
     return (
         <div>
             <br />
+            <div>····●·○·●·○·●·○·●·○·●····</div>
             <div>💬**ДЛЯ ЗАКАЗА**💬</div>
             <div>🚀Telegram: @onlinemyprice</div>
+            <br />
+            <div>✈️ **Наши отправки**</div>
+            <div>🚀Telegram: t.me/mobilochka_otparvki</div>
         </div>
     )
 }
